4463790ac00a841b2ffcb2e07023856c00c552b9
sandeep26dc <sandeep26dc@gmail.com>
2026-07-22 08:36:34 +0400

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+// Top-level build file where plugin versions are defined for all sub-modules
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
