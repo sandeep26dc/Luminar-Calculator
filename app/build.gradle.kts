63d3bd5dc2bf29ef9e3f9f3cecdf462261d634ba
sandeep26dc <sandeep26dc@gmail.com>
2026-07-22 09:09:33 +0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
 
-    // Live math evaluation parser
+    // Extended Math Parser (Exp4j with custom functions)
     implementation("net.objecthunter:exp4j:0.4.8")
 }
