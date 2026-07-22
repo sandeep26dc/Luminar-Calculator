53305ced841fb4015ead8af5864358b36826efe9
sandeep26dc <sandeep26dc@gmail.com>
2026-07-22 08:19:33 +0400

    Create Color.kt

---
+++ app/src/main/java/com/example/luminarcalculator/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.luminarcalculator.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LightBackground = Color(0xFFE8EEF5)
+val LightButtonSurface = Color(0xFFFFFFFF)
+val LightButtonText = Color(0xFF4A5568)
+val LightSecondaryText = Color(0xFF718096)
+
+val DarkBackground = Color(0xFF0D131D)
+val DarkButtonSurface = Color(0xFF1E2638)
+val DarkButtonText = Color(0xFFE2E8F0)
+val DarkSecondaryText = Color(0xFF94A3B8)
+
+val ElectricBlue = Color(0xFF007AFF)
+val AccentText = Color(0xFFFFFFFF)
