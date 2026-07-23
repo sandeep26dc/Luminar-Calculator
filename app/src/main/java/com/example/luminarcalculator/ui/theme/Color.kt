bddc24086bac6532ff62c4292d9980dd025e892b
sandeep26dc <sandeep26dc@gmail.com>
2026-07-23 07:43:16 +0400

    Update Color.kt

--- app/src/main/java/com/example/luminarcalculator/ui/theme/Color.kt
+++ app/src/main/java/com/example/luminarcalculator/ui/theme/Color.kt
@@ -2,15 +2,15 @@ package com.example.luminarcalculator.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val LightBackground = Color(0xFFE8EEF5)
-val LightButtonSurface = Color(0xFFFFFFFF)
-val LightButtonText = Color(0xFF4A5568)
-val LightSecondaryText = Color(0xFF718096)
+// Obsidian & Cyber Cyan Palette
+val ObsidianBg = Color(0xFF070A0F)
+val SurfaceGlassTop = Color(0xFF131B26)
+val SurfaceGlassBottom = Color(0xFF090D14)
 
-val DarkBackground = Color(0xFF0D131D)
-val DarkButtonSurface = Color(0xFF1E2638)
-val DarkButtonText = Color(0xFFE2E8F0)
-val DarkSecondaryText = Color(0xFF94A3B8)
+val NeonCyan = Color(0xFF00E5FF)
+val ElectricBlue = Color(0xFF0088FF)
+val NeonEmerald = Color(0xFF00FF9D)
 
-val ElectricBlue = Color(0xFF007AFF)
-val AccentText = Color(0xFFFFFFFF)
+val TextPrimary = Color(0xFFFFFFFF)
+val TextSecondary = Color(0x80FFFFFF)
+val GlassBorder = Color(0x3300E5FF)
