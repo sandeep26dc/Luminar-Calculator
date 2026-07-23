eeb2c48325140e7ff8f0c0306afbf4975a3dd1a0
sandeep26dc <sandeep26dc@gmail.com>
2026-07-23 08:44:19 +0400

    Create Theme.kt

---
+++ app/src/main/java/com/example/luminarcalculator/ui/theme/Theme.kt
@@ -0,0 +1,21 @@
+package com.example.luminarcalculator.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme Palette
+val LightBackground = Color(0xFFE3EDF7)
+val LightKeyNum = Color(0xFFE3EDF7)
+val LightKeyOp = Color(0xFFD0DFEE)
+val LightKeyTop = Color(0xFFD0DFEE)
+val LightKeyAccent = Color(0xFF00E5FF)
+val LightTextPrimary = Color(0xFF1E293B)
+val LightTextSecondary = Color(0xFF64748B)
+
+// Dark Theme Palette
+val DarkBackground = Color(0xFF0D1117)
+val DarkKeyNum = Color(0xFF14181F)
+val DarkKeyOp = Color(0xFF1C222D)
+val DarkKeyTop = Color(0xFF1C222D)
+val DarkKeyAccent = Color(0xFF00E5FF)
+val DarkTextPrimary = Color(0xFFFFFFFF)
+val DarkTextSecondary = Color(0xFF94A3B8)
