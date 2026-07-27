391c3c8373df2d2bca92482de7e81774070a7727
Sandeep Som <sandeep26dc@gmail.com>
2026-07-27 11:47:53 +0400

    Create CalculatorDatabase.kt

---
+++ com/example/luminarcalculator/data/CalculatorDatabase.kt
@@ -0,0 +1,29 @@
+package com.example.luminarcalculator.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [CalculationEntity::class], version = 1, exportSchema = false)
+abstract class CalculatorDatabase : RoomDatabase() {
+
+    abstract fun calculationDao(): CalculationDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: CalculatorDatabase? = null
+
+        fun getDatabase(context: Context): CalculatorDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    CalculatorDatabase::class.java,
+                    "luminar_calculator_db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
