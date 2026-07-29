825655bcb29fc02454b1e24fcf9bf4df8f5de58b
Sandeep Som <sandeep26dc@gmail.com>
2026-07-29 11:53:03 +0400

    Update MainNavigation.kt

--- app/src/main/java/com/example/luminarcalculator/ui/MainNavigation.kt
+++ app/src/main/java/com/example/luminarcalculator/ui/MainNavigation.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
